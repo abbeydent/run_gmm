f1cca9c8e3ea57b9d1c820f00ecf0d1e9fe5a1c8
Abbey Dent <adent@g.clemson.edu>
2019-05-28 15:29:37 -0400

    trying gaussian mixture model

--- run_gmm.py
+++ run_gmm.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 
 
 dataset = pd.read_csv("dataset.csv", header = None)
@@ -14,3 +15,7 @@ plt.savefig("scatterplot.png")
 kmeans_predictions = KMeans(n_clusters=3).fit_predict(dataset)
 plt.scatter(dataset[0],dataset[1], c=kmeans_predictions)
 plt.savefig("scatterplot_kmeans_3.png")
+
+gaussian_predictions = GaussianMixture(n_components=3).fit(dataset).predict(dataset)
+plt.scatter(dataset[0], dataset[1], c=gaussian_predictions)
+plt.savefig("scatterplot_gaussian_3.png")
