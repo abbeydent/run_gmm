4c5f2ad2327d062f433e81215170c6b9ac9ff99d
Abbey Dent <adent@g.clemson.edu>
2019-05-28 15:43:52 -0400

    scores for K-means clusters

--- run_gmm.py
+++ run_gmm.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
+from sklearn import metrics
 
 
 dataset = pd.read_csv("dataset.csv", header = None)
@@ -12,10 +13,31 @@ print(dataset.head())
 plt.scatter(dataset[0], dataset[1])
 plt.savefig("scatterplot.png")
 
-kmeans_predictions = KMeans(n_clusters=3).fit_predict(dataset)
-plt.scatter(dataset[0],dataset[1], c=kmeans_predictions)
+# kmeans_predictions = KMeans(n_clusters=3).fit_predict(dataset)
+# plt.scatter(dataset[0],dataset[1], c=kmeans_predictions)
+# plt.savefig("scatterplot_kmeans_3.png")
+
+kmeans_predictions_4 = KMeans(n_clusters=4).fit_predict(dataset)
+plt.scatter(dataset[0], dataset[1], c=kmeans_predictions_4)
+plt.savefig("scatterplot_kmeans_4.png")
+print("K-mean 4 clusters")
+print(metrics.silhouette_score(dataset, kmeans_predictions_4))
+
+kmeans_predictions_3 = KMeans(n_clusters=3).fit_predict(dataset)
+plt.scatter(dataset[0], dataset[1], c=kmeans_predictions_3)
 plt.savefig("scatterplot_kmeans_3.png")
+print("K-mean 3 clusters")
+print(metrics.silhouette_score(dataset, kmeans_predictions_3))
+
+kmeans_predictions_2 = KMeans(n_clusters=2).fit_predict(dataset)
+plt.scatter(dataset[0], dataset[1], c=kmeans_predictions_2)
+plt.savefig("scatterplot_kmeans_2.png")
+print("K-mean 2 clusters")
+print(metrics.silhouette_score(dataset, kmeans_predictions_2))
+
+
 
 gaussian_predictions = GaussianMixture(n_components=3).fit(dataset).predict(dataset)
 plt.scatter(dataset[0], dataset[1], c=gaussian_predictions)
 plt.savefig("scatterplot_gaussian_3.png")
+
