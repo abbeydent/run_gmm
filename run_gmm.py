ed1c8283577e36360d785d1d4087355c473b455f
Abbey Dent <adent@g.clemson.edu>
2019-05-28 15:24:45 -0400

    trying with k-means clustering

--- run_gmm.py
+++ run_gmm.py
@@ -1,9 +1,16 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
+from sklearn.cluster import KMeans
+
+
 dataset = pd.read_csv("dataset.csv", header = None)
 
 print(dataset.head())
 
 plt.scatter(dataset[0], dataset[1])
 plt.savefig("scatterplot.png")
+
+kmeans_predictions = KMeans(n_clusters=3).fit_predict(dataset)
+plt.scatter(dataset[0],dataset[1], c=kmeans_predictions)
+plt.savefig("scatterplot_kmeans_3.png")
